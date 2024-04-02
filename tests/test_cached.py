e7034779aaa436c1b0dfc799baaee4cf9fb567cc
Galtozzy <galtozzy+git@gmail.com>
2024-04-02 21:12:57 +0200

    Fix tests

--- tests/test_cached.py
+++ tests/test_cached.py
@@ -1,17 +1,24 @@
 import sys
+from typing import TypeVar
 
 import pytest
 from pytest_mock import MockerFixture
+from typing_extensions import ParamSpec
 
 from py_cachify import cached
+from py_cachify.backend.cached import async_cached, sync_cached
 from py_cachify.backend.exceptions import CachifyInitError
 
 
-def sync_function(arg1, arg2):
+R = TypeVar('R')
+P = ParamSpec('P')
+
+
+def sync_function(arg1: int, arg2: int) -> int:
     return arg1 + arg2
 
 
-async def async_function(arg1, arg2):
+async def async_function(arg1: int, arg2: int) -> int:
     return arg1 + arg2
 
 
@@ -43,3 +50,15 @@ def test_cached_decorator_check_cachify_init():
     sync_function_wrapped = cached(key='test_key')(sync_function)
     with pytest.raises(CachifyInitError, match='Cachify is not initialized, did you forget to call `init_cachify`?'):
         sync_function_wrapped(3, 4)
+
+
+def test_sync_cached_preserves_type_annotations(init_cachify_fixture):
+    func = sync_cached(key='test_key_{arg1}')(async_function)
+    for name, clz in [('arg1', int), ('arg2', int), ('return', int)]:
+        assert func.__annotations__[name] == clz
+
+
+def test_async_cached_preserves_type_annotations(init_cachify_fixture):
+    func = async_cached(key='test_key_{arg1}')(async_function)
+    for name, clz in [('arg1', int), ('arg2', int), ('return', int)]:
+        assert func.__annotations__[name] == clz
