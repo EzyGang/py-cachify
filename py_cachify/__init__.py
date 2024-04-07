a585b1e7522faef28446275f14bccc8ef12ad50f
Sergey Kuzmich <accounts@kuzmi.ch>
2024-04-07 14:25:36 +0200

    feat: Add get_cachify function to lib module in __init__.py
    Add the get_cachify function to the lib module in __init__.py for direct access to caching functionality.

--- py_cachify/__init__.py
+++ py_cachify/__init__.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 from .backend.cached import cached
 from .backend.exceptions import CachifyInitError, CachifyLockError
-from .backend.lib import init_cachify
+from .backend.lib import init_cachify, get_cachify
 from .backend.lock import once
 
 
@@ -10,6 +10,7 @@ __all__ = [
     'CachifyInitError',
     'CachifyLockError',
     'init_cachify',
+    'get_cachify',
     'cached',
     'once',
     'sync',
